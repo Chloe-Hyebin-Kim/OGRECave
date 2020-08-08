baiwen1979/ogre

add 1.11 api docs

api/1.11/class_ogre_1_1_g_l_texture_manager.js
@@ -1,0 +1,73 @@
+var class_ogre_1_1_g_l_texture_manager =
+[
+    [ "ResourceCreateOrRetrieveResult", "class_ogre_1_1_g_l_texture_manager.html#af484f2b60c8d9bc87f37fa8af8e630c5", null ],
+    [ "ResourceMap", "class_ogre_1_1_g_l_texture_manager.html#a849d25862f43feec07252f0ae139c3e4", null ],
+    [ "ResourceWithGroupMap", "class_ogre_1_1_g_l_texture_manager.html#a31a40854bf7ade620d6632550f2c474b", null ],
+    [ "ResourceHandleMap", "class_ogre_1_1_g_l_texture_manager.html#a6d9424ed93bc679b5543ddc9fc9fe41b", null ],
+    [ "ResourceMapIterator", "class_ogre_1_1_g_l_texture_manager.html#a4f7da59507a5a8550ea97615ff9afff0", null ],
+    [ "GLTextureManager", "class_ogre_1_1_g_l_texture_manager.html#abb689751d7c5c15e6d8696539e180f51", null ],
+    [ "~GLTextureManager", "class_ogre_1_1_g_l_texture_manager.html#a2fe8c19ca4de4722f1fd618e3181554a", null ],
+    [ "getNativeFormat", "class_ogre_1_1_g_l_texture_manager.html#ad866ae0c8780ced34a47f1bce4e45676", null ],
+    [ "isHardwareFilteringSupported", "class_ogre_1_1_g_l_texture_manager.html#a9c37a081d9c634bdf0ce2e3b025de35b", null ],
+    [ "create", "class_ogre_1_1_g_l_texture_manager.html#ae63b06c1fd6d1266b4c5dacc35e521b4", null ],
+    [ "getByName", "class_ogre_1_1_g_l_texture_manager.html#ac046eb7c0c2c99d7e90635e0cb92b8dc", null ],
+    [ "createOrRetrieve", "class_ogre_1_1_g_l_texture_manager.html#aa0533d51bf31cc8dd5868585d3530dfd", null ],
+    [ "createOrRetrieve", "class_ogre_1_1_g_l_texture_manager.html#ac3f365e1d71fbe328637a681171d7acd", null ],
+    [ "prepare", "class_ogre_1_1_g_l_texture_manager.html#aa5effb36ddd157b7b137cccd120ea55a", null ],
+    [ "prepare", "class_ogre_1_1_g_l_texture_manager.html#a4350b30d83790f208a9fdd1197ce9c95", null ],
+    [ "load", "class_ogre_1_1_g_l_texture_manager.html#a23fa50345ee27fa2911e36edbd5df489", null ],
+    [ "load", "class_ogre_1_1_g_l_texture_manager.html#a379c3ff20c97fcd13db3bc7841b4b560", null ],
+    [ "loadImage", "class_ogre_1_1_g_l_texture_manager.html#aae5cce977c3523af14bb17b3a954effd", null ],
+    [ "loadRawData", "class_ogre_1_1_g_l_texture_manager.html#a3083fb17ce51843aecdc0382d2d717f6", null ],
+    [ "createManual", "class_ogre_1_1_g_l_texture_manager.html#aafa494d2704fdff05708db9db4bac3a0", null ],
+    [ "createManual", "class_ogre_1_1_g_l_texture_manager.html#aac423b67b8c1aa00cd6a3f91e73bfcec", null ],
+    [ "setPreferredIntegerBitDepth", "class_ogre_1_1_g_l_texture_manager.html#aaaa53651124f3773fbcf6ae2b4a6fbe5", null ],
+    [ "getPreferredIntegerBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a49a47b8919269342243265df3bd1e1fc", null ],
+    [ "setPreferredFloatBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a77d097944def071bd10e1c2a03fc5b2c", null ],
+    [ "getPreferredFloatBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a2a5f6c92d4f91eb1eaa0ccf9b73f8c83", null ],
+    [ "setPreferredBitDepths", "class_ogre_1_1_g_l_texture_manager.html#af2cae9ecc99635a541df72ae7cdd337a", null ],
+    [ "isFormatSupported", "class_ogre_1_1_g_l_texture_manager.html#a65730ea50a81401518b4cbf6bb6a0a73", null ],
+    [ "isEquivalentFormatSupported", "class_ogre_1_1_g_l_texture_manager.html#abbbab1db9a334e39c2114ee3a21b013f", null ],
+    [ "setDefaultNumMipmaps", "class_ogre_1_1_g_l_texture_manager.html#aec8fc8fae795d7410867f72f00813c79", null ],
+    [ "getDefaultNumMipmaps", "class_ogre_1_1_g_l_texture_manager.html#af30b8543978558d0a083778790f1fab2", null ],
+    [ "_getWarningTexture", "class_ogre_1_1_g_l_texture_manager.html#a9ef8419603b3aad6ce7545682ee12c7c", null ],
+    [ "getSingleton", "class_ogre_1_1_g_l_texture_manager.html#ac6fb858ee86f3b7b75de32e3e04ee5c8", null ],
+    [ "getSingletonPtr", "class_ogre_1_1_g_l_texture_manager.html#a3a55789d982b34bdd9daa27650360653", null ],
+    [ "createResource", "class_ogre_1_1_g_l_texture_manager.html#a700088a63eabb0f6cb1fac96686db464", null ],
+    [ "setMemoryBudget", "class_ogre_1_1_g_l_texture_manager.html#a532e96fbeac065b4a7866b4302a90618", null ],
+    [ "getMemoryBudget", "class_ogre_1_1_g_l_texture_manager.html#abd807a5a8f5754af45fc60db40b455d5", null ],
+    [ "getMemoryUsage", "class_ogre_1_1_g_l_texture_manager.html#ac401b57afcc205728a3821065aef6238", null ],
+    [ "unload", "class_ogre_1_1_g_l_texture_manager.html#ae81042ca7b4622594e30158950813e73", null ],
+    [ "unload", "class_ogre_1_1_g_l_texture_manager.html#a2514952116c93b5b5eba051492d2caa9", null ],
+    [ "unloadAll", "class_ogre_1_1_g_l_texture_manager.html#a1fe343406d15d5367cfc4f86ba589b55", null ],
+    [ "unloadAll", "class_ogre_1_1_g_l_texture_manager.html#a93b40c858020f2bbbcebcd4dc72a6321", null ],
+    [ "reloadAll", "class_ogre_1_1_g_l_texture_manager.html#aa9608544a7beb1b0e43835877ab62788", null ],
+    [ "reloadAll", "class_ogre_1_1_g_l_texture_manager.html#a84e25cc5ad09ff7567d1fed7429433a3", null ],
+    [ "unloadUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#aeaa1410333b7bcea14b00c360112a528", null ],
+    [ "reloadUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#a4dda8323b265a00638dd29a4fa6510bc", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#a257ae8a9ac7538581173b7ddcf7b3f19", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#a21344fefa1d01e03fdc0093018b7690f", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#a6871e913a62e1b419438e3169bc72bd3", null ],
+    [ "removeAll", "class_ogre_1_1_g_l_texture_manager.html#a6ff50fee06a678837cbeeea75e4372a5", null ],
+    [ "removeUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#a6ce966e769770ce9f90d77eddc2c30a3", null ],
+    [ "getResourceByName", "class_ogre_1_1_g_l_texture_manager.html#a735d1109ace47edb7a1cf6d32c14e141", null ],
+    [ "getByHandle", "class_ogre_1_1_g_l_texture_manager.html#ab1c354fd2294c7c50b3f3e9ce42543f7", null ],
+    [ "resourceExists", "class_ogre_1_1_g_l_texture_manager.html#a586351b1eed6a528c99e4d66b248c08b", null ],
+    [ "resourceExists", "class_ogre_1_1_g_l_texture_manager.html#a6a58b96feb67c25e123ccc3b287fcd2c", null ],
+    [ "_notifyResourceTouched", "class_ogre_1_1_g_l_texture_manager.html#a40769092850d5b296e1d1a9190631700", null ],
+    [ "_notifyResourceLoaded", "class_ogre_1_1_g_l_texture_manager.html#af1a871ac005af0be0b8c1857a4f8eea9", null ],
+    [ "_notifyResourceUnloaded", "class_ogre_1_1_g_l_texture_manager.html#a54d1843a43aebb13542312cd17134e6a", null ],
+    [ "getScriptPatterns", "class_ogre_1_1_g_l_texture_manager.html#a356d42e699011a2d86770c9b3ee55e6d", null ],
+    [ "parseScript", "class_ogre_1_1_g_l_texture_manager.html#a266d959fecaf0568270acfbf024e0b4b", null ],
+    [ "getLoadingOrder", "class_ogre_1_1_g_l_texture_manager.html#a3ae7c3ae5494fc088fa8a17e736fef6b", null ],
+    [ "getResourceType", "class_ogre_1_1_g_l_texture_manager.html#abc20acc2d57fd224b218a07d802e8c1a", null ],
+    [ "setVerbose", "class_ogre_1_1_g_l_texture_manager.html#a82b76280229c498b8d66f64b2a848447", null ],
+    [ "getVerbose", "class_ogre_1_1_g_l_texture_manager.html#ae071093e2681923ddf1d661087b44e4d", null ],
+    [ "getResourcePool", "class_ogre_1_1_g_l_texture_manager.html#ad18cc3f0a4b3e0553cfe715204f15359", null ],
+    [ "destroyResourcePool", "class_ogre_1_1_g_l_texture_manager.html#aa8b761abb06d74347e9636550307ccd9", null ],
+    [ "destroyResourcePool", "class_ogre_1_1_g_l_texture_manager.html#a00a7fe59aaaa4acdfda07edc704516ee", null ],
+    [ "destroyAllResourcePools", "class_ogre_1_1_g_l_texture_manager.html#a7913c36c02ef03007ff1a1c76ddf7710", null ],
+    [ "getResourceIterator", "class_ogre_1_1_g_l_texture_manager.html#a4d3289d4636168230da11c1c9794db59", null ],
+    [ "GLRenderSystem", "class_ogre_1_1_g_l_texture_manager.html#a70060cc0754d0fdda64492d59c22c859", null ],
+    [ "OGRE_AUTO_MUTEX", "class_ogre_1_1_g_l_texture_manager.html#abbb523a827bc6ea90d3b5ec80f0cf770", null ]
+];

Chloe-Hyebin-Kim/OGRECave

api/1.11/class_ogre_1_1_g_l_texture_manager.js
@@ -1,0 +1,76 @@
+var class_ogre_1_1_g_l_texture_manager =
+[
+    [ "ResourceCreateOrRetrieveResult", "class_ogre_1_1_g_l_texture_manager.html#af484f2b60c8d9bc87f37fa8af8e630c5", null ],
+    [ "ResourceMap", "class_ogre_1_1_g_l_texture_manager.html#a849d25862f43feec07252f0ae139c3e4", null ],
+    [ "ResourceWithGroupMap", "class_ogre_1_1_g_l_texture_manager.html#a31a40854bf7ade620d6632550f2c474b", null ],
+    [ "ResourceHandleMap", "class_ogre_1_1_g_l_texture_manager.html#a6d9424ed93bc679b5543ddc9fc9fe41b", null ],
+    [ "ResourceMapIterator", "class_ogre_1_1_g_l_texture_manager.html#a4f7da59507a5a8550ea97615ff9afff0", null ],
+    [ "GLTextureManager", "class_ogre_1_1_g_l_texture_manager.html#abb689751d7c5c15e6d8696539e180f51", null ],
+    [ "~GLTextureManager", "class_ogre_1_1_g_l_texture_manager.html#a2fe8c19ca4de4722f1fd618e3181554a", null ],
+    [ "getNativeFormat", "class_ogre_1_1_g_l_texture_manager.html#ad866ae0c8780ced34a47f1bce4e45676", null ],
+    [ "isHardwareFilteringSupported", "class_ogre_1_1_g_l_texture_manager.html#a9c37a081d9c634bdf0ce2e3b025de35b", null ],
+    [ "createSampler", "class_ogre_1_1_g_l_texture_manager.html#a0e25bec00bdbd7618a264b67c204c07b", null ],
+    [ "getSampler", "class_ogre_1_1_g_l_texture_manager.html#aacb0663130a096fcf49e5dbedbcca92c", null ],
+    [ "create", "class_ogre_1_1_g_l_texture_manager.html#ae63b06c1fd6d1266b4c5dacc35e521b4", null ],
+    [ "getByName", "class_ogre_1_1_g_l_texture_manager.html#a91734962d27d948eaa9ce6dac0b771a6", null ],
+    [ "createOrRetrieve", "class_ogre_1_1_g_l_texture_manager.html#a406e3deba98785c0183d9e87271078d2", null ],
+    [ "createOrRetrieve", "class_ogre_1_1_g_l_texture_manager.html#ac3f365e1d71fbe328637a681171d7acd", null ],
+    [ "prepare", "class_ogre_1_1_g_l_texture_manager.html#aa5effb36ddd157b7b137cccd120ea55a", null ],
+    [ "prepare", "class_ogre_1_1_g_l_texture_manager.html#a4350b30d83790f208a9fdd1197ce9c95", null ],
+    [ "load", "class_ogre_1_1_g_l_texture_manager.html#a23fa50345ee27fa2911e36edbd5df489", null ],
+    [ "load", "class_ogre_1_1_g_l_texture_manager.html#a379c3ff20c97fcd13db3bc7841b4b560", null ],
+    [ "loadImage", "class_ogre_1_1_g_l_texture_manager.html#aae5cce977c3523af14bb17b3a954effd", null ],
+    [ "loadRawData", "class_ogre_1_1_g_l_texture_manager.html#a3083fb17ce51843aecdc0382d2d717f6", null ],
+    [ "createManual", "class_ogre_1_1_g_l_texture_manager.html#aafa494d2704fdff05708db9db4bac3a0", null ],
+    [ "createManual", "class_ogre_1_1_g_l_texture_manager.html#aac423b67b8c1aa00cd6a3f91e73bfcec", null ],
+    [ "setPreferredIntegerBitDepth", "class_ogre_1_1_g_l_texture_manager.html#aaaa53651124f3773fbcf6ae2b4a6fbe5", null ],
+    [ "getPreferredIntegerBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a49a47b8919269342243265df3bd1e1fc", null ],
+    [ "setPreferredFloatBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a77d097944def071bd10e1c2a03fc5b2c", null ],
+    [ "getPreferredFloatBitDepth", "class_ogre_1_1_g_l_texture_manager.html#a2a5f6c92d4f91eb1eaa0ccf9b73f8c83", null ],
+    [ "setPreferredBitDepths", "class_ogre_1_1_g_l_texture_manager.html#af2cae9ecc99635a541df72ae7cdd337a", null ],
+    [ "isFormatSupported", "class_ogre_1_1_g_l_texture_manager.html#a65730ea50a81401518b4cbf6bb6a0a73", null ],
+    [ "isEquivalentFormatSupported", "class_ogre_1_1_g_l_texture_manager.html#abbbab1db9a334e39c2114ee3a21b013f", null ],
+    [ "setDefaultNumMipmaps", "class_ogre_1_1_g_l_texture_manager.html#aec8fc8fae795d7410867f72f00813c79", null ],
+    [ "getDefaultNumMipmaps", "class_ogre_1_1_g_l_texture_manager.html#af30b8543978558d0a083778790f1fab2", null ],
+    [ "_getWarningTexture", "class_ogre_1_1_g_l_texture_manager.html#a9ef8419603b3aad6ce7545682ee12c7c", null ],
+    [ "getDefaultSampler", "class_ogre_1_1_g_l_texture_manager.html#a711218e2968d7e52b1114cced356c8e8", null ],
+    [ "getSingleton", "class_ogre_1_1_g_l_texture_manager.html#ac6fb858ee86f3b7b75de32e3e04ee5c8", null ],
+    [ "getSingletonPtr", "class_ogre_1_1_g_l_texture_manager.html#a3a55789d982b34bdd9daa27650360653", null ],
+    [ "createResource", "class_ogre_1_1_g_l_texture_manager.html#a700088a63eabb0f6cb1fac96686db464", null ],
+    [ "setMemoryBudget", "class_ogre_1_1_g_l_texture_manager.html#a532e96fbeac065b4a7866b4302a90618", null ],
+    [ "getMemoryBudget", "class_ogre_1_1_g_l_texture_manager.html#abd807a5a8f5754af45fc60db40b455d5", null ],
+    [ "getMemoryUsage", "class_ogre_1_1_g_l_texture_manager.html#ac401b57afcc205728a3821065aef6238", null ],
+    [ "unload", "class_ogre_1_1_g_l_texture_manager.html#a62ddf3760bd9871b25ca016732e149d1", null ],
+    [ "unload", "class_ogre_1_1_g_l_texture_manager.html#a2514952116c93b5b5eba051492d2caa9", null ],
+    [ "unloadAll", "class_ogre_1_1_g_l_texture_manager.html#a1fe343406d15d5367cfc4f86ba589b55", null ],
+    [ "unloadAll", "class_ogre_1_1_g_l_texture_manager.html#a93b40c858020f2bbbcebcd4dc72a6321", null ],
+    [ "reloadAll", "class_ogre_1_1_g_l_texture_manager.html#aa9608544a7beb1b0e43835877ab62788", null ],
+    [ "reloadAll", "class_ogre_1_1_g_l_texture_manager.html#a84e25cc5ad09ff7567d1fed7429433a3", null ],
+    [ "unloadUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#aeaa1410333b7bcea14b00c360112a528", null ],
+    [ "reloadUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#a4dda8323b265a00638dd29a4fa6510bc", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#a257ae8a9ac7538581173b7ddcf7b3f19", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#aea812804e59454cd29ee531ebab56b1b", null ],
+    [ "remove", "class_ogre_1_1_g_l_texture_manager.html#a6871e913a62e1b419438e3169bc72bd3", null ],
+    [ "removeAll", "class_ogre_1_1_g_l_texture_manager.html#a6ff50fee06a678837cbeeea75e4372a5", null ],
+    [ "removeUnreferencedResources", "class_ogre_1_1_g_l_texture_manager.html#a6ce966e769770ce9f90d77eddc2c30a3", null ],
+    [ "getResourceByName", "class_ogre_1_1_g_l_texture_manager.html#a0154dd3f0ed6e8665eecab30d2535101", null ],
+    [ "getByHandle", "class_ogre_1_1_g_l_texture_manager.html#ab1c354fd2294c7c50b3f3e9ce42543f7", null ],
+    [ "resourceExists", "class_ogre_1_1_g_l_texture_manager.html#a3fdbd35468f69e6f6662bae7ec84af21", null ],
+    [ "resourceExists", "class_ogre_1_1_g_l_texture_manager.html#a6a58b96feb67c25e123ccc3b287fcd2c", null ],
+    [ "_notifyResourceTouched", "class_ogre_1_1_g_l_texture_manager.html#a40769092850d5b296e1d1a9190631700", null ],
+    [ "_notifyResourceLoaded", "class_ogre_1_1_g_l_texture_manager.html#af1a871ac005af0be0b8c1857a4f8eea9", null ],
+    [ "_notifyResourceUnloaded", "class_ogre_1_1_g_l_texture_manager.html#a54d1843a43aebb13542312cd17134e6a", null ],
+    [ "getScriptPatterns", "class_ogre_1_1_g_l_texture_manager.html#a356d42e699011a2d86770c9b3ee55e6d", null ],
+    [ "parseScript", "class_ogre_1_1_g_l_texture_manager.html#a266d959fecaf0568270acfbf024e0b4b", null ],
+    [ "getLoadingOrder", "class_ogre_1_1_g_l_texture_manager.html#a3ae7c3ae5494fc088fa8a17e736fef6b", null ],
+    [ "getResourceType", "class_ogre_1_1_g_l_texture_manager.html#abc20acc2d57fd224b218a07d802e8c1a", null ],
+    [ "setVerbose", "class_ogre_1_1_g_l_texture_manager.html#a82b76280229c498b8d66f64b2a848447", null ],
+    [ "getVerbose", "class_ogre_1_1_g_l_texture_manager.html#ae071093e2681923ddf1d661087b44e4d", null ],
+    [ "getResourcePool", "class_ogre_1_1_g_l_texture_manager.html#ad18cc3f0a4b3e0553cfe715204f15359", null ],
+    [ "destroyResourcePool", "class_ogre_1_1_g_l_texture_manager.html#aa8b761abb06d74347e9636550307ccd9", null ],
+    [ "destroyResourcePool", "class_ogre_1_1_g_l_texture_manager.html#a00a7fe59aaaa4acdfda07edc704516ee", null ],
+    [ "destroyAllResourcePools", "class_ogre_1_1_g_l_texture_manager.html#a7913c36c02ef03007ff1a1c76ddf7710", null ],
+    [ "getResourceIterator", "class_ogre_1_1_g_l_texture_manager.html#a4d3289d4636168230da11c1c9794db59", null ],
+    [ "GLRenderSystem", "class_ogre_1_1_g_l_texture_manager.html#a70060cc0754d0fdda64492d59c22c859", null ],
+    [ "OGRE_AUTO_MUTEX", "class_ogre_1_1_g_l_texture_manager.html#abbb523a827bc6ea90d3b5ec80f0cf770", null ]
+];